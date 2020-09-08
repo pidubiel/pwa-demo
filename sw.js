143249b8b152b4412352b12c6969f183bcf6c637
Piotr Dubiel <piotrdubiel@icloud.com>
2020-09-08 10:17:42 +0200

    add service worker

---
+++ sw.js
@@ -0,0 +1,7 @@
+// install service worker
+self.addEventListener('install', (evt) => console.log('service worker has been installed'));
+
+// activate sw
+self.addEventListener('activate', (evt) => {
+  console.log('service worker has been activated');
+});
